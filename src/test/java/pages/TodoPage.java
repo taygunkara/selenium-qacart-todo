ec512d74011e6de1b8b1162c51e2fe28600987b5
Taygun Kara <kara.taygun@gmail.com>
2024-07-09 17:21:09 +0300

    new methods for todo page

--- src/test/java/pages/TodoPage.java
+++ src/test/java/pages/TodoPage.java
@@ -7,12 +7,15 @@ import utils.ConfigUtils;
 public class TodoPage {
 
     private final static By plusIcon = By.cssSelector("[data-testid=\"add\"]");
-
+    private final static By deleteIcon = By.cssSelector("[data-testid=\"delete\"]");
+    private final static By welcomeDisplay = By.cssSelector("[data-testid=\"welcome\"]");
+    private final static By todoDisplay = By.cssSelector("[data-testid=\"todo-text\"]");
+    private final static By noTodosDisplay = By.cssSelector("[data-testid=\"no-todos\"]");
 
     private static TodoPage todoPage;
     private TodoPage(){}
 
-    public static TodoPage todoPage(){
+    public static TodoPage getInstance(){
         if (todoPage == null){
             todoPage = new TodoPage();
         }
@@ -25,6 +28,22 @@ public class TodoPage {
 
     public void clickPlusIcon(WebDriver driver){
         driver.findElement(plusIcon).click();
+    }
+
+    public void clickDeleteIcon(WebDriver driver){
+        driver.findElement(deleteIcon).click();
+    }
+
+    public boolean isWelcomeDisplayed(WebDriver driver){
+        return driver.findElement(welcomeDisplay).isDisplayed();
+    }
+
+    public boolean isTodoDisplayed(WebDriver driver){
+        return driver.findElement(todoDisplay).isDisplayed();
+    }
+
+    public boolean isNoTodosDisplayed(WebDriver driver){
+        return driver.findElement(noTodosDisplay).isDisplayed();
     }
 
 }
