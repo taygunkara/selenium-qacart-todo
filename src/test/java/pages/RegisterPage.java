8a8d398ba26743d48e5ce1c96ecf7bf1a22a2e71
Taygun Kara <kara.taygun@gmail.com>
2024-07-09 15:46:12 +0300

    methods for register and load page

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,11 +1,13 @@
 package pages;
 
+import models.User;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.ConfigUtils;
 
 public class RegisterPage {
 
 
-    // ELEMENTS
     private final static By firstnameInput = By.cssSelector("[data-testid=\"first-name\"]");
     private final static By lastnameInput = By.cssSelector("[data-testid=\"last-name\"]");
     private final static By emailInput = By.cssSelector("[data-testid=\"email\"]");
@@ -13,7 +15,7 @@ public class RegisterPage {
     private final static By confirmPasswordInput = By.cssSelector("[data-testid=\"confirm-password\"]");
     private final static By signUpButton = By.cssSelector("[data-testid=\"submit\"]");
 
-    // SINGLETON
+
     private static RegisterPage registerPage;
     private RegisterPage(){};
 
@@ -24,8 +26,16 @@ public class RegisterPage {
         return registerPage;
     }
 
-    // METHODS
+    public void register(WebDriver driver, User user){
+        driver.findElement(firstnameInput).sendKeys(user.getFirstName());
+        driver.findElement(lastnameInput).sendKeys(user.getLastName());
+        driver.findElement(emailInput).sendKeys(user.getEmail());
+        driver.findElement(passwordInput).sendKeys(user.getPassword());
+        driver.findElement(confirmPasswordInput).sendKeys(user.getPassword());
+        driver.findElement(signUpButton).click();
+    }
 
-
-
+    public void load(WebDriver driver){
+        driver.get(ConfigUtils.getInstance().getBaseURL() + "/signup");
+    }
 }
