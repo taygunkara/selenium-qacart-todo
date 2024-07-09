c66ddb8bcdfe492d30c8279ff4ccf2d45198d36d
Taygun Kara <kara.taygun@gmail.com>
2024-07-09 13:38:26 +0300

    support for multiple browsers

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -3,6 +3,7 @@ package factory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
@@ -10,13 +11,23 @@ public class DriverFactory {
 
     public static WebDriver initializeDriver(){
         WebDriver driver;
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "CHROME");
+
+        switch (browser){
+            case "CHROME" ->{
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "FIREFOX" ->{
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            default -> throw new RuntimeException("Browser is not supported!");
+        }
+
         driver.get("https://todo.qacart.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
     }
-
-
 }
