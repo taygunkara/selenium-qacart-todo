f87f2c9bcb7b6b26f5b67d5a40f4ddbe4f37af0d
Taygun Kara <kara.taygun@gmail.com>
2024-07-09 14:00:49 +0300

    register page elements

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -1,4 +1,23 @@
 package pages;
 
+import org.openqa.selenium.By;
+
 public class RegisterPage {
+
+
+    // ELEMENTS
+    private final static By firstnameInput = By.cssSelector("[data-testid=\"first-name\"]");
+    private final static By lastnameInput = By.cssSelector("[data-testid=\"last-name\"]");
+    private final static By emailInput = By.cssSelector("[data-testid=\"email\"]");
+    private final static By passwordInput = By.cssSelector("[data-testid=\"password\"]");
+    private final static By confirmPasswordInput = By.cssSelector("[data-testid=\"confirm-password\"]");
+    private final static By signUpButton = By.cssSelector("[data-testid=\"submit\"]");
+
+    // SINGLETON
+
+
+    // METHODS
+
+
+
 }
