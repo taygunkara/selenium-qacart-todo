b41e8e959a293211a337b54d12b17055d22116e6
Taygun Kara <kara.taygun@gmail.com>
2024-07-09 13:28:29 +0300

    browser driver initialization to factory from base

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,24 +1,16 @@
 package base;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import factory.DriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
 
 public class BaseTest {
-
     WebDriver driver;
-
     @BeforeMethod
     public void setUp(){
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://todo.qacart.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver = DriverFactory.initializeDriver();
     }
 
     @AfterMethod
--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -1,4 +1,22 @@
 package factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class DriverFactory {
+
+    public static WebDriver initializeDriver(){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://todo.qacart.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        return driver;
+    }
+
+
 }
